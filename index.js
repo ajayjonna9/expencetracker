5300c384e03a412392da952668ac701d3b63ced7
ajayjonna9 <100123937+ajayjonna9@users.noreply.github.com>
2022-11-08 13:03:29 +0530

    promises

--- index.js
+++ index.js
@@ -1,78 +1,102 @@
-let form = document.querySelector("#form");
-let list = document.querySelector("#list");
-let expence = document.querySelector("input[name=expence]");
-let des = document.querySelector("input[name=description]");
-let category = document.querySelector("select[name=category]");
+const arr = [
+  {
+    title: "post one",
+    body: "body of post one",
+    timestamp: new Date().getSeconds(),
+  },
+  {
+    title: "post two",
+    body: "body of post two",
+    timestamp: new Date().getSeconds(),
+  },
+  {
+    title: "post three",
+    body: "body of post three",
+    timestamp: new Date().getSeconds(),
+  },
+];
 
-form.addEventListener("submit", onSubmit);
-function onSubmit(e) {
-  e.preventDefault();
-  console.log(expence.value, des.value, category.value);
-  createElements(expence.value, des.value, category.value);
-  createObj(expence.value, des.value, category.value);
+function getpost() {
+  setTimeout(() => {
+    let inner = "";
+    console.log(arr.length);
+    if (arr.length > 0) {
+      arr.forEach((post) => {
+        inner += `<li>${post.title} created  ${
+          new Date().getSeconds() - post.timestamp
+        } secoends ago</li>`;
+        document.body.innerHTML = inner;
+      });
+    } else {
+      document.body.innerHTML = inner;
+    }
+  }, 0);
 }
-function createElements(expence, description, category) {
-  let inner = `<li class="list">${expence}-${description}-${category} 
-    <button class="edit">Edit</button><button class="delete">Delete</button></li>`;
-  list.innerHTML = list.innerHTML + inner;
-}
-function createObj(expence, description, category) {
-  let Obj = {
-    expence: expence,
-    description: description,
-    category: category,
-  };
-  let myObj = JSON.stringify(Obj);
-  localStorage.setItem(Obj.description, myObj);
-}
-
-list.addEventListener("click", onClick);
-function onClick(e) {
-  console.log(e.target);
-  if (e.target.className === "edit") {
-    //console.log("but");
-    console.log(e.target.parentElement);
-    let index1 = e.target.parentElement.innerText.indexOf("-");
-    let temp = e.target.parentElement.innerText.substring(index1 + 1);
-    let index2 = temp.indexOf("-");
-    //console.log(temp);
-    //console.log(index2);
-    let expenceValue = e.target.parentElement.innerText.substring(
-      index1 + 1,
-      index2 + index1 + 1
-    );
-    //console.log(expenceValue);
-    let obj = JSON.parse(localStorage.getItem(expenceValue));
-    expence.value = obj.expence;
-    des.value = obj.description;
-    category.value = obj.category;
-    e.target.parentElement.remove();
-  }
-  if (e.target.className === "delete") {
-    //console.log("but");
-    console.log(e.target.parentElement);
-    let index1 = e.target.parentElement.innerText.indexOf("-");
-    let temp = e.target.parentElement.innerText.substring(index1 + 1);
-    let index2 = temp.indexOf("-");
-    //console.log(temp);
-    //console.log(index2);
-    let expenceValue = e.target.parentElement.innerText.substring(
-      index1 + 1,
-      index2 + index1 + 1
-    );
-
-    e.target.parentElement.remove();
-    localStorage.removeItem(expenceValue);
-  }
-}
-document.addEventListener("DOMContentLoaded", domloaded);
-function domloaded() {
-  var key = Object.keys(localStorage);
-  key.forEach((key) => {
-    let text = JSON.parse(localStorage.getItem(key));
-    createElements(text.expence, text.description, text.category);
-    //const li = document.createElement("li");
-    //createElementNode(text.name, text.email);
-    // Add text node with input values
+let timer = 0;
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      timer += 1000;
+      arr.push({ ...post, timestamp: new Date().getSeconds() });
+      console.log("created at ", new Date());
+      console.log(arr);
+      let error = false;
+      if (!error) {
+        resolve();
+      } else {
+        reject("error");
+      }
+    }, 0);
   });
 }
+const post = {
+  title: "post four",
+  body: "body of post four",
+};
+function updatePost() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("updated at", new Date());
+      resolve();
+    }, 2000);
+  });
+}
+
+/*createPost(post)
+  .then(getpost)
+  .catch((err) => console.log(err));*/
+
+function deletePost() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let ele = arr.pop();
+      console.log(arr);
+      if (ele) {
+        resolve();
+      } else {
+        clearInterval(id);
+
+        reject("error");
+      }
+    }, 1000);
+  });
+}
+/*let id = setInterval(() => {
+  deletePost()
+    .then(getpost)
+    .catch((err) => {
+      console.log(err);
+    });
+
+}, 1000);*/
+Promise.all([createPost(post), updatePost(), deletePost()]).then((msg) => {});
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("hi");
+  }, 2000);
+});
+const promise2 = 444;
+const promise3 = Promise.resolve("hello");
+Promise.all([promise1, promise2, promise3]).then((value) => {
+  console.log(value);
+});
