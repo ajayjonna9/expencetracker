ec753a9040f4ffed00f01cb12949f251353e432f
ajayjonna9 <100123937+ajayjonna9@users.noreply.github.com>
2022-11-11 16:39:09 +0530

    expenceTrackerWithNetworkcalls

--- index.js
+++ index.js
@@ -1,75 +1,101 @@
-console.log("person1 shows ticket");
-console.log("person2 shows ticket");
-const premovie = async () => {
-  const promiseWifeBringTicket = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("ticket");
-    }, 2000);
-  });
-  let getpopcorn = new Promise((resolve, reject) => {
-    resolve(`popcorn`);
-  });
-  let getCoolDrinks = new Promise((resolve, reject) => {
-    resolve(` cooldrinks`);
-  });
-  let getbutter = new Promise((resolve, reject) => {
-    resolve(` butter`);
-  });
+let form = document.querySelector("#form");
 
-  let [ticket, popcorn, butter, cooldrinks] = await Promise.all([
-    promiseWifeBringTicket,
-    getpopcorn,
-    getbutter,
-    getCoolDrinks,
-  ]);
-  console.log(`${ticket},${popcorn},${butter},${cooldrinks}`);
-  /*let wait = await promiseWifeBringTicket;
+let list = document.querySelector("#list");
 
-  console.log("husband:we should get in");
-  console.log("wife:i want popcorn");
-  let popcorn = await getpopcorn;
-  console.log(`husband: here is the ${popcorn}`);
-  console.log("husband:we should get in");
-  console.log("wife:i want butter");
-  let butter = await getbutter;
-  console.log(`husband: here is the ${butter}`);
+let expence = document.querySelector("input[name=expence]");
 
-  console.log("husband:we should get in");
-  console.log("wife:i want cooldrinks");
-  let cooldrinks = await getCoolDrinks;
-  console.log(`husband: here is the ${cooldrinks}`);
-  console.log("husband:we should get in");
-  console.log("wife:ok");*/
+let des = document.querySelector("input[name=description]");
 
-  return ticket;
-};
-/*const promiseWifeBringTicket = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("ticket");
-  }, 2000);
-});
-const getpopcorn = promiseWifeBringTicket.then((ticket) => {
-  console.log("husband:we should get in");
-  console.log("wife:i want popcorn");
-  return new Promise((resolve, reject) => {
-    resolve(`${ticket} popcorn`);
-  });
-});
-const getbutter = getpopcorn.then((msg) => {
-  console.log("husband:we should get in");
-  console.log("wife:i want butter");
-  return new Promise((resolve, reject) => {
-    resolve(`${msg} butter`);
-  });
-});
-const getCoolDrinks = getbutter.then((msg) => {
-  console.log("husband:we should get in");
-  console.log("wife:i want cooldrinks");
-  return new Promise((resolve, reject) => {
-    resolve(`${msg} cooldrinks`);
-  });
-});
-getCoolDrinks.then((msg) => console.log(`${msg}`));*/
-premovie().then((m) => console.log(`person3 shows ${m}`));
-console.log("person4 shows ticket");
-console.log("person5 shows ticket");
+let category = document.querySelector("select[name=category]");
+
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  console.log(expence.value, des.value, category.value);
+
+  createObj(expence.value, des.value, category.value);
+  expence.value = "";
+  des.value = "";
+  category.value = "select";
+}
+
+function createElements(expence, description, category, id) {
+  let inner = `<li id="${id}">${expence}-${description}-${category} 
+
+    <button class="edit" onclick=onEdit('${id}','${expence}','${description}','${category}') )>Edit</button><button class="delete" onclick=onDelete('${id}')>Delete</button></li>`;
+
+  list.innerHTML = list.innerHTML + inner;
+}
+
+function createObj(expence, description, category) {
+  let Obj = {
+    expence: expence,
+
+    description: description,
+
+    category: category,
+  };
+  axios
+    .post(
+      `https://crudcrud.com/api/29957ed71b804dfc8dbdb99f85587249/expencedata`,
+      Obj
+    )
+    .then((res) => {
+      createElements(
+        res.data.expence,
+        res.data.description,
+        res.data.category,
+        res.data._id
+      );
+    })
+    .catch((err) => {
+      document.body.innerHTML = `<h3>${err}</h3>`;
+    });
+}
+function onDelete(id) {
+  console.log("hi");
+  axios
+    .delete(
+      `https://crudcrud.com/api/29957ed71b804dfc8dbdb99f85587249/expencedata/${id}`
+    )
+    .then((res) => {
+      console.log(res);
+
+      removeUser(id);
+    })
+    .catch((err) => {
+      document.body.innerHTML = `<h3>${err}</h3>`;
+    });
+}
+function removeUser(id) {
+  const deletedElemen = document.getElementById(id);
+
+  console.log(deletedElemen);
+  deletedElemen.remove();
+}
+//list.addEventListener("click", onClick);
+function onEdit(ele_id, exp, description, cat) {
+  expence.value = exp;
+  des.value = description;
+  category.value = cat;
+  onDelete(ele_id);
+}
+
+document.addEventListener("DOMContentLoaded", domloaded);
+
+function domloaded() {
+  axios
+    .get(
+      `https://crudcrud.com/api/29957ed71b804dfc8dbdb99f85587249/expencedata`
+    )
+    .then((res) => {
+      res.data.forEach((key) => {
+        createElements(key.expence, key.description, key.category, key._id);
+      });
+    })
+    .catch((err) => {
+      document.body.innerHTML = `<h3>${err}</h3>`;
+    });
+}
